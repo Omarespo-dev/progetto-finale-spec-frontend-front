a4749304e3cc49ea237d537e80039e04fcaa1ad7
Omarespo-dev <omaresposito21@gmail.com>
2025-06-24 17:59:37 +0200

    fixed css layout Hero Section and added Typewriter

--- src/components/Home/Hero Section/HeroSection.jsx
+++ src/components/Home/Hero Section/HeroSection.jsx
@@ -1,4 +1,9 @@
+//css
 import "../../../style/HeroSection.css"
+
+//typewriter
+import Typewriter from 'typewriter-effect';
+
 
 export default function HeroSection() {
     return (
@@ -9,12 +14,29 @@ export default function HeroSection() {
             </video>
 
             <div className="flex-item-hero">
-                <h1>Confronta <span className="highlight">Smartphone</span> in Tempo Reale</h1>
-                
-                <div className="set-input-btn-hero">
-                    <input type="text" placeholder="Inizia a digitare qui per confrontare"/>
-                    <button>Confronta</button>
+                <h1>
 
+                    {/* TypeWriter preso da libreria */}
+                    <Typewriter
+                        options={{
+                            loop: true,
+                            delay: 80,
+                            deleteSpeed: 80,
+                        }}
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString('Confronta <span class="highlight">Smartphone</span> in Tempo Reale')
+                                .pauseFor(3000)
+                                .deleteAll()
+                                .start();
+                        }}
+                    />
+
+                </h1>
+
+                <div className="set-input-btn-hero">
+                    <input type="text" placeholder="Inizia a digitare qui per confrontare" />
+                    <button>Confronta</button>
                 </div>
 
             </div>
