87a25a69adcb1d529259cf5e97c39b03abf9f967
Omarespo-dev <omaresposito21@gmail.com>
2025-06-24 20:54:17 +0200

    Added layout Section for Homepage --COMPLETE LAYOUT HOMEPAGE ---DO COMPARE PAGE AND DETAIL PAGE LAYOUT
    LOGIC IMPLEMENT LATER LAYOUT

--- src/components/Home/Down Section/SectionProductCompare.jsx
+++ src/components/Home/Down Section/SectionProductCompare.jsx
@@ -1,7 +1,45 @@
 
+//importo css
+import "../../../style/SectionProductCompare.css"
+
+//importo icona
+import { CiHeart } from "react-icons/ci";
+
+
 
 export default function SectionProductCompare() {
   return (
-    <div>SectionProductCompare</div>
+    <div className="container-smartphone-compare">
+
+      <div className="contaier-flex-smartphone">
+
+        <div className="smartphone-evidenza-container">
+          <h2>Smartphone in <span className="highlight">Evidenza </span></h2>
+          <p>Le nostre selezioni più consigliate, scelte appositamente per te.</p>
+          <button>Vedi Tutti gli Smartphone</button>
+        </div>
+
+        <div className="smartphone-card">
+          <div className="card-evidenza">
+            <section className="set-cuore-card">
+              <CiHeart />
+            </section>
+
+            <section className="set-img-card">
+              <img src="../../../images/apple-iphone-15-pro.jpg" alt="" />
+            </section>
+
+            <section className="set-description">
+              <p>Apple iPhone 14 Pro Max 128GB Deep Purple </p>
+              <button>Aggiungi al Confronto</button>
+            </section>
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </div>
   )
 }
